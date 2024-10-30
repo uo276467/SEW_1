7de0594f6794bc1cb865ef54dfab1894c42c2741
uo276467 <98962580+uo276467@users.noreply.github.com>
2024-10-30 22:19:14 +0100

    Add files via upload

--- js/memoria.js
+++ js/memoria.js
@@ -21,5 +21,82 @@ class Memoria{
         this.firstCard = null
         this.secondCard = null
     }
-    
+    shuffleElements(){
+        // Verificación de que elements y elements.elements están definidos y son un array
+        if (!this.elements || !Array.isArray(this.elements.elements) || this.elements.elements.length === 0) {
+            console.error("No hay elementos disponibles para mezclar.");
+            return;
+        }
+
+        let array = this.elements.elements;
+
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            // Intercambio de elementos en posiciones i y j
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+    unflipCards(){
+        this.lockBoard = true;
+    }
+    resetBoard(){
+        this.firstCard = null
+        this.secondCard = null
+        this.hasFlippedCard = false
+        this.lockBoard = false
+    }
+    checkForMath(){
+        if(this.firstCard === this.secondCard)
+            this.disableCards()
+        else
+            this.unflipCards()
+    }
+    disableCards(){
+        //datastate = revealed
+        this.resetBoard();
+    }
+    createElements(){
+        const section = document.querySelector("section")
+
+        this.elements.elements.forEach(item => {
+            // Crear el artículo
+            const article = document.createElement("article")
+            article.setAttribute("data-element", item.element)
+            article.classList.add("card")
+
+            // Crear el encabezado
+            const text = document.createElement("h3")
+            text.textContent = "Tarjeta de memoria"
+            article.appendChild(text)
+
+            // Crear la imagen
+            const image = document.createElement("img")
+            image.src = item.source
+            image.alt = item.element
+            image.style.display = "none" 
+            article.appendChild(image)
+
+            // Añadir el artículo a la sección
+            section.appendChild(article)
+        });
+
+        this.addEventListeners()
+    }
+    addEventListeners(){
+        const articles = document.querySelectorAll("section article");
+
+        articles.forEach(card => {
+            card.onclick = this.flipCard.bind(card, this)
+        });
+    }
+    flipCard(card){
+        const text = card.querySelector("h3")
+        const image = card.querySelector("img")
+        text.style.display = "none"
+        image.style.display = "block"
+    }
 }
+
+const memoria = new Memoria()
+memoria.shuffleElements()
+memoria.createElements()
