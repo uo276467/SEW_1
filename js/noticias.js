1e5cfcb7f8a16908876ecd9d64802f4c41eb6703
uo276467 <98962580+uo276467@users.noreply.github.com>
2024-11-21 14:54:04 +0100

    Add files via upload

--- js/noticias.js
+++ js/noticias.js
@@ -13,22 +13,49 @@ class Noticias{
         var tipoTexto = /text.*/;
       if (archivo.type.match(tipoTexto)) 
         {
-          var lector = new FileReader();
-          lector.onload = function (evento) {
-            //El evento "onload" se lleva a cabo cada vez que se completa con éxito una operación de lectura
-            //La propiedad "result" es donde se almacena el contenido del archivo
-            //Esta propiedad solamente es válida cuando se termina la operación de lectura
-            areaVisualizacion.innerText = lector.result;
-            }      
-          lector.readAsText(archivo);
-          }
+            this.showInputFile(archivo)
+        }
       else {
           errorArchivo.innerText = "Error : ¡¡¡ Archivo no válido !!!";
           }    
     }
+    showInputFile(archivo){
+        var lector = new FileReader();
+        lector.readAsText(archivo)
+        lector.onload = function (archivo) {
+            var parrafos = lector.result.split("_")
+            const noticias = parrafos.reduce((result, current, index) => {
+                // Identificar a qué grupo pertenece el string actual
+                const groupIndex = Math.floor(index / 3);
+              
+                // Si el grupo no existe, inicializarlo
+                if (!result[groupIndex]) {
+                  result[groupIndex] = '';
+                }
+              
+                // Añadir el string actual al grupo correspondiente, separados por "@"
+                result[groupIndex] += (result[groupIndex] ? '@' : '') + current;
+              
+                return result;
+            }, []);  
+
+            console.log(noticias)
+
+            noticias.forEach(noticia => {
+                var lineas = noticia.split("@")
+                var stringHtml = "<article>"
+                stringHtml += "<h3>" + lineas[0] + "</h3>"
+                stringHtml += "<p>" + lineas[1] + "</p>"
+                stringHtml += "<p>" + lineas[2] + "</p></article>"
+
+                $("section").append(stringHtml);
+            });
+        }    
+    }
     addNews(){
-        var nuevaNoticia = document.querySelector("#nueva").textContent
-        $("pre").append(nuevaNoticia);
+        var nuevaNoticia = document.querySelector("section input[type='text']").value
+        var areaVisualizacion = document.querySelector("pre");
+        areaVisualizacion.innerText += nuevaNoticia + "\n"
     }
 }
 var noticias = new Noticias()
